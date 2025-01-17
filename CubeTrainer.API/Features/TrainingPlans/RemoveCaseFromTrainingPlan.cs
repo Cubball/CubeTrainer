d04a2846af6781b4763f578ab122d5824b31049b
Cubball <138378823+Cubball@users.noreply.github.com>
2025-01-17 21:29:28 +0200

    feat: add use case to remove case from training plan

---
+++ CubeTrainer.API/Features/TrainingPlans/RemoveCaseFromTrainingPlan.cs
@@ -0,0 +1,50 @@
+using System.Security.Claims;
+using CubeTrainer.API.Common;
+using CubeTrainer.API.Common.Endpoints;
+using CubeTrainer.API.Common.Exceptions;
+using CubeTrainer.API.Database;
+using CubeTrainer.API.Entities;
+using FluentValidation;
+using FluentValidation.Results;
+using Microsoft.EntityFrameworkCore;
+
+namespace CubeTrainer.API.Features.TrainingPlans;
+
+internal static class RemoveCaseFromTrainingPlan
+{
+    public sealed record Response(Guid Id);
+
+    public sealed class Endpoint : IEndpoint
+    {
+        public void Map(IEndpointRouteBuilder builder)
+        {
+            builder
+                .MapDelete("/training-plans/{id:guid}/cases/{caseId:guid}", Handle)
+                .WithTags("Training Plans")
+                .RequireAuthorization();
+        }
+    }
+
+    private static async Task<IResult> Handle(
+        Guid id,
+        Guid caseId,
+        AppDbContext context,
+        ClaimsPrincipal user,
+        CancellationToken cancellationToken)
+    {
+        var userId = (user.Claims.FirstOrDefault(static c => c.Type == Constants.Auth.UserIdClaimType)?.Value)
+            ?? throw new UnauthorizedException("User not found");
+        var trainingPlanCase = await context.TrainingPlanCases
+            .Include(tpc => tpc.TrainingPlan)
+            .FirstOrDefaultAsync(tpc => tpc.TrainingPlanId == id && tpc.CaseId == caseId, cancellationToken)
+            ?? throw new NotFoundException("Case not found");
+        if (trainingPlanCase.TrainingPlan.UserId != userId)
+        {
+            throw new NotFoundException("Case not found");
+        }
+
+        context.TrainingPlanCases.Remove(trainingPlanCase);
+        await context.SaveChangesAsync(cancellationToken);
+        return Results.Ok(new Response(id));
+    }
+}
