eb2359909abeb8c402a6844dac264c29f2c0e6b7
Cubball <138378823+Cubball@users.noreply.github.com>
2025-02-09 18:00:45 +0200

    feat: split work into threads

--- CubeTrainer.Cube/Kociemba/Phase1/PruneTables/EOAndUDSlicePruneTable.cs
+++ CubeTrainer.Cube/Kociemba/Phase1/PruneTables/EOAndUDSlicePruneTable.cs
@@ -7,8 +7,6 @@ internal class EOAndUDSlicePruneTable
 {
     private const int UDSlicePossibleCoordinatesCount = 495; // redefined here as a constant for efficiency
 
-    private static readonly Queue<(ushort, ushort, byte)> Queue = new();
-
     // FIXME:
     public readonly byte[] _moves = new byte[EdgeOrientationCoordinate.PossibleCoordinatesCount * UDSliceCoordinate.PossibleCoordinatesCount];
 
@@ -24,29 +22,92 @@ internal class EOAndUDSlicePruneTable
             table._moves[i] = byte.MaxValue;
         }
 
-        for (var eo = 0; eo < EdgeOrientationCoordinate.PossibleCoordinatesCount; eo++)
+        var thread1 = new Thread(() =>
         {
-            for (var ud = 0; ud < UDSliceCoordinate.PossibleCoordinatesCount; ud++)
+            var queue = new Queue<(ushort, ushort, byte)>();
+            for (var eo = 0; eo < 512; eo++)
             {
-                var moves = BFS(eo, ud, eoMoveTable, udMoveTable);
-                table.SetMoves(eo, ud, moves);
+                for (var ud = 0; ud < UDSliceCoordinate.PossibleCoordinatesCount; ud++)
+                {
+                    var moves = BFS(eo, ud, eoMoveTable, udMoveTable, queue);
+                    table.SetMoves(eo, ud, moves);
+                }
+
+                Console.WriteLine($"Finished {eo}");
             }
+        });
+        thread1.Start();
 
-            Console.WriteLine($"Finished {eo}");
-        }
+        var thread2 = new Thread(() =>
+        {
+            var queue = new Queue<(ushort, ushort, byte)>();
+            for (var eo = 512; eo < 1024; eo++)
+            {
+                for (var ud = 0; ud < UDSliceCoordinate.PossibleCoordinatesCount; ud++)
+                {
+                    var moves = BFS(eo, ud, eoMoveTable, udMoveTable, queue);
+                    table.SetMoves(eo, ud, moves);
+                }
 
+                Console.WriteLine($"Finished {eo}");
+            }
+        });
+        thread2.Start();
+
+        var thread3 = new Thread(() =>
+        {
+            var queue = new Queue<(ushort, ushort, byte)>();
+            for (var eo = 1024; eo < 1536; eo++)
+            {
+                for (var ud = 0; ud < UDSliceCoordinate.PossibleCoordinatesCount; ud++)
+                {
+                    var moves = BFS(eo, ud, eoMoveTable, udMoveTable, queue);
+                    table.SetMoves(eo, ud, moves);
+                }
+
+                Console.WriteLine($"Finished {eo}");
+            }
+        });
+        thread3.Start();
+
+        var thread4 = new Thread(() =>
+        {
+            var queue = new Queue<(ushort, ushort, byte)>();
+            for (var eo = 1536; eo < 2048; eo++)
+            {
+                for (var ud = 0; ud < UDSliceCoordinate.PossibleCoordinatesCount; ud++)
+                {
+                    var moves = BFS(eo, ud, eoMoveTable, udMoveTable, queue);
+                    table.SetMoves(eo, ud, moves);
+                }
+
+                Console.WriteLine($"Finished {eo}");
+            }
+        });
+        thread4.Start();
+
+        thread1.Join();
+        thread2.Join();
+        thread3.Join();
+        thread4.Join();
         return table;
     }
 
-    private static byte BFS(int startEo, int startUd, MoveTable<ushort> eoMoveTable, MoveTable<ushort> udMoveTable)
+    private static byte BFS(
+        int startEo,
+        int startUd,
+        MoveTable<ushort> eoMoveTable,
+        MoveTable<ushort> udMoveTable,
+        Queue<(ushort, ushort, byte)> queue)
     {
-        Queue.Clear();
+        queue.Clear();
         Span<bool> visited = stackalloc bool[EdgeOrientationCoordinate.PossibleCoordinatesCount * UDSliceCoordinate.PossibleCoordinatesCount];
-        Queue.Enqueue(((ushort)startEo, (ushort)startUd, 0));
+        visited.Clear();
+        queue.Enqueue(((ushort)startEo, (ushort)startUd, 0));
         visited[GetIndex(startEo, startUd)] = true;
-        while (Queue.Count > 0)
+        while (queue.Count > 0)
         {
-            var (eo, ud, moves) = Queue.Dequeue();
+            var (eo, ud, moves) = queue.Dequeue();
             if (eo == 0 && ud == 0)
             {
                 return moves;
@@ -57,130 +118,131 @@ internal class EOAndUDSlicePruneTable
             if (!visited[GetIndex(nextEo, nextUd)])
             {
                 visited[GetIndex(nextEo, nextUd)] = true;
-                Queue.Enqueue((nextEo, nextUd, (byte)(moves + 1)));
+                queue.Enqueue((nextEo, nextUd, (byte)(moves + 1)));
             }
 
             (nextEo, nextUd) = (eoMoveTable.GetValue(eo, 'R', 2), udMoveTable.GetValue(ud, 'R', 2));
             if (!visited[GetIndex(nextEo, nextUd)])
             {
                 visited[GetIndex(nextEo, nextUd)] = true;
-                Queue.Enqueue((nextEo, nextUd, (byte)(moves + 1)));
+                queue.Enqueue((nextEo, nextUd, (byte)(moves + 1)));
             }
 
             (nextEo, nextUd) = (eoMoveTable.GetValue(eo, 'R', 3), udMoveTable.GetValue(ud, 'R', 3));
             if (!visited[GetIndex(nextEo, nextUd)])
             {
                 visited[GetIndex(nextEo, nextUd)] = true;
-                Queue.Enqueue((nextEo, nextUd, (byte)(moves + 1)));
+                queue.Enqueue((nextEo, nextUd, (byte)(moves + 1)));
             }
 
             (nextEo, nextUd) = (eoMoveTable.GetValue(eo, 'L', 1), udMoveTable.GetValue(ud, 'L', 1));
             if (!visited[GetIndex(nextEo, nextUd)])
             {
                 visited[GetIndex(nextEo, nextUd)] = true;
-                Queue.Enqueue((nextEo, nextUd, (byte)(moves + 1)));
+                queue.Enqueue((nextEo, nextUd, (byte)(moves + 1)));
             }
 
             (nextEo, nextUd) = (eoMoveTable.GetValue(eo, 'L', 2), udMoveTable.GetValue(ud, 'L', 2));
             if (!visited[GetIndex(nextEo, nextUd)])
             {
                 visited[GetIndex(nextEo, nextUd)] = true;
-                Queue.Enqueue((nextEo, nextUd, (byte)(moves + 1)));
+                queue.Enqueue((nextEo, nextUd, (byte)(moves + 1)));
             }
 
             (nextEo, nextUd) = (eoMoveTable.GetValue(eo, 'L', 3), udMoveTable.GetValue(ud, 'L', 3));
             if (!visited[GetIndex(nextEo, nextUd)])
             {
                 visited[GetIndex(nextEo, nextUd)] = true;
-                Queue.Enqueue((nextEo, nextUd, (byte)(moves + 1)));
+                queue.Enqueue((nextEo, nextUd, (byte)(moves + 1)));
             }
 
             (nextEo, nextUd) = (eoMoveTable.GetValue(eo, 'U', 1), udMoveTable.GetValue(ud, 'U', 1));
             if (!visited[GetIndex(nextEo, nextUd)])
             {
                 visited[GetIndex(nextEo, nextUd)] = true;
-                Queue.Enqueue((nextEo, nextUd, (byte)(moves + 1)));
+                queue.Enqueue((nextEo, nextUd, (byte)(moves + 1)));
             }
 
             (nextEo, nextUd) = (eoMoveTable.GetValue(eo, 'U', 2), udMoveTable.GetValue(ud, 'U', 2));
             if (!visited[GetIndex(nextEo, nextUd)])
             {
                 visited[GetIndex(nextEo, nextUd)] = true;
-                Queue.Enqueue((nextEo, nextUd, (byte)(moves + 1)));
+                queue.Enqueue((nextEo, nextUd, (byte)(moves + 1)));
             }
 
             (nextEo, nextUd) = (eoMoveTable.GetValue(eo, 'U', 3), udMoveTable.GetValue(ud, 'U', 3));
             if (!visited[GetIndex(nextEo, nextUd)])
             {
                 visited[GetIndex(nextEo, nextUd)] = true;
-                Queue.Enqueue((nextEo, nextUd, (byte)(moves + 1)));
+                queue.Enqueue((nextEo, nextUd, (byte)(moves + 1)));
             }
 
             (nextEo, nextUd) = (eoMoveTable.GetValue(eo, 'D', 1), udMoveTable.GetValue(ud, 'D', 1));
             if (!visited[GetIndex(nextEo, nextUd)])
             {
                 visited[GetIndex(nextEo, nextUd)] = true;
-                Queue.Enqueue((nextEo, nextUd, (byte)(moves + 1)));
+                queue.Enqueue((nextEo, nextUd, (byte)(moves + 1)));
             }
 
             (nextEo, nextUd) = (eoMoveTable.GetValue(eo, 'D', 2), udMoveTable.GetValue(ud, 'D', 2));
             if (!visited[GetIndex(nextEo, nextUd)])
             {
                 visited[GetIndex(nextEo, nextUd)] = true;
-                Queue.Enqueue((nextEo, nextUd, (byte)(moves + 1)));
+                queue.Enqueue((nextEo, nextUd, (byte)(moves + 1)));
             }
 
             (nextEo, nextUd) = (eoMoveTable.GetValue(eo, 'D', 3), udMoveTable.GetValue(ud, 'D', 3));
             if (!visited[GetIndex(nextEo, nextUd)])
             {
                 visited[GetIndex(nextEo, nextUd)] = true;
-                Queue.Enqueue((nextEo, nextUd, (byte)(moves + 1)));
+                queue.Enqueue((nextEo, nextUd, (byte)(moves + 1)));
             }
 
             (nextEo, nextUd) = (eoMoveTable.GetValue(eo, 'F', 1), udMoveTable.GetValue(ud, 'F', 1));
             if (!visited[GetIndex(nextEo, nextUd)])
             {
                 visited[GetIndex(nextEo, nextUd)] = true;
-                Queue.Enqueue((nextEo, nextUd, (byte)(moves + 1)));
+                queue.Enqueue((nextEo, nextUd, (byte)(moves + 1)));
             }
 
             (nextEo, nextUd) = (eoMoveTable.GetValue(eo, 'F', 2), udMoveTable.GetValue(ud, 'F', 2));
             if (!visited[GetIndex(nextEo, nextUd)])
             {
                 visited[GetIndex(nextEo, nextUd)] = true;
-                Queue.Enqueue((nextEo, nextUd, (byte)(moves + 1)));
+                queue.Enqueue((nextEo, nextUd, (byte)(moves + 1)));
             }
 
             (nextEo, nextUd) = (eoMoveTable.GetValue(eo, 'F', 3), udMoveTable.GetValue(ud, 'F', 3));
             if (!visited[GetIndex(nextEo, nextUd)])
             {
                 visited[GetIndex(nextEo, nextUd)] = true;
-                Queue.Enqueue((nextEo, nextUd, (byte)(moves + 1)));
+                queue.Enqueue((nextEo, nextUd, (byte)(moves + 1)));
             }
 
             (nextEo, nextUd) = (eoMoveTable.GetValue(eo, 'B', 1), udMoveTable.GetValue(ud, 'B', 1));
             if (!visited[GetIndex(nextEo, nextUd)])
             {
                 visited[GetIndex(nextEo, nextUd)] = true;
-                Queue.Enqueue((nextEo, nextUd, (byte)(moves + 1)));
+                queue.Enqueue((nextEo, nextUd, (byte)(moves + 1)));
             }
 
             (nextEo, nextUd) = (eoMoveTable.GetValue(eo, 'B', 2), udMoveTable.GetValue(ud, 'B', 2));
             if (!visited[GetIndex(nextEo, nextUd)])
             {
                 visited[GetIndex(nextEo, nextUd)] = true;
-                Queue.Enqueue((nextEo, nextUd, (byte)(moves + 1)));
+                queue.Enqueue((nextEo, nextUd, (byte)(moves + 1)));
             }
 
             (nextEo, nextUd) = (eoMoveTable.GetValue(eo, 'B', 3), udMoveTable.GetValue(ud, 'B', 3));
             if (!visited[GetIndex(nextEo, nextUd)])
             {
                 visited[GetIndex(nextEo, nextUd)] = true;
-                Queue.Enqueue((nextEo, nextUd, (byte)(moves + 1)));
+                queue.Enqueue((nextEo, nextUd, (byte)(moves + 1)));
             }
         }
 
-        throw new Exception("Was not able to find a solved state");
+        Console.WriteLine($"Was not able to find a solved state ({startEo}; {startUd})");
+        return byte.MaxValue;
     }
 
     public byte GetMoves(int eoCoordinate, int udCoordinate)
