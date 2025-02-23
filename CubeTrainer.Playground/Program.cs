158642462e093579e48eff3f77b7ec73ff6e9933
Cubball <138378823+Cubball@users.noreply.github.com>
2025-02-23 19:23:59 +0200

    feat: remove timer

--- CubeTrainer.Playground/Program.cs
+++ CubeTrainer.Playground/Program.cs
@@ -1,11 +1,7 @@
-﻿using System.Diagnostics;
-using CubeTrainer.Cube;
+﻿using CubeTrainer.Cube;
 
 var scramble = MoveSequence.Random(20);
 var cube = RubiksCube.Scrambled(scramble);
-var stopwatch = Stopwatch.StartNew();
 var solution = RubiksCubeSolver.FindSolution(cube);
-stopwatch.Stop();
 Console.WriteLine($"Scramble: {scramble}");
 Console.WriteLine($"Solution: {solution}");
-Console.WriteLine($"Time: {stopwatch.Elapsed}");
