5cc509b350f431352986487e8290c7368429b337
Cubball <138378823+Cubball@users.noreply.github.com>
2025-03-11 20:00:02 +0200

    feat: add register page

--- CubeTrainer.Frontend/src/App.tsx
+++ CubeTrainer.Frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router'
 import Login from './features/auth/pages/Login'
+import Register from './features/auth/pages/Register'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   )
--- CubeTrainer.Frontend/src/features/auth/pages/Register.tsx
+++ CubeTrainer.Frontend/src/features/auth/pages/Register.tsx
@@ -8,10 +8,16 @@ import loader from '../../../assets/loader.svg'
 import { useNavigate } from 'react-router'
 import AuthContainer from '../components/AuthContainer'
 
-const schema = z.object({
-  email: z.string().email('Please enter a valid email'),
-  password: z.string().min(8, 'Please enter a valid password'),
-})
+const schema = z
+  .object({
+    email: z.string().email('Please enter a valid email'),
+    password: z.string().min(8, 'Please enter a valid password'),
+    repeatPassword: z.string(),
+  })
+  .refine((data) => data.password === data.repeatPassword, {
+    message: 'Passwords do not match',
+    path: ['repeatPassword'],
+  })
 
 type FormData = z.infer<typeof schema>
 
@@ -25,7 +31,7 @@ const Login = () => {
     resolver: zodResolver(schema),
   })
   const navigate = useNavigate()
-  const mutation = useMutation({
+  const loginMutation = useMutation({
     mutationFn: (data: FormData) =>
       axiosInstance.post('/login', data, {
         params: {
@@ -33,22 +39,35 @@ const Login = () => {
         },
       }),
     onSuccess: () => navigate('/'),
+    onError: () => setError('root', { message: 'An unknown error occurred' }),
+  })
+  const registerMutation = useMutation({
+    mutationFn: async (data: FormData) => {
+      await axiosInstance.post('/register', data)
+      return data
+    },
+    onSuccess: (data) => loginMutation.mutate(data),
     onError: (error) => {
-      if (axios.isAxiosError(error) && error.response?.status === 401) {
-        setError('root', { message: 'Incorrect email or password' })
-      } else {
-        setError('root', { message: 'An unknown error occurred' })
+      if (
+        axios.isAxiosError(error) &&
+        error.response?.status === 400 &&
+        error.response.data?.errors?.DuplicateEmail
+      ) {
+        setError('email', { message: 'Email already taken' })
+        return
       }
+
+      setError('root', { message: 'An unknown error occurred' })
     },
   })
 
   return (
     <AuthContainer>
       <h1 className="mb-4 text-center text-xl font-semibold">
-        Log in to your account
+        Create an account
       </h1>
       <form
-        onSubmit={handleSubmit((data) => mutation.mutate(data))}
+        onSubmit={handleSubmit((data) => registerMutation.mutate(data))}
         className="flex flex-col items-center gap-4"
       >
         <div>
@@ -68,12 +87,21 @@ const Login = () => {
           />
           <p className="text-red-500">{errors.password?.message}</p>
         </div>
-        {mutation.isPending ? (
+        <div>
+          <input
+            type="password"
+            placeholder="Repeat password"
+            {...register('repeatPassword')}
+            className="rounded-sm border border-gray-400 p-2"
+          />
+          <p className="text-red-500">{errors.repeatPassword?.message}</p>
+        </div>
+        {registerMutation.isPending ? (
           <img src={loader} width="40" />
         ) : (
           <>
             <button className="w-full cursor-pointer rounded-sm bg-gray-800 p-2 text-white">
-              Login
+              Sign up
             </button>
             {errors.root && (
               <p className="text-red-500">{errors.root.message}</p>
@@ -82,14 +110,14 @@ const Login = () => {
         )}
       </form>
       <p className="text-center">
-        Don't have an account?
+        Already have an account?
         <br />
         <button
           type="button"
           className="cursor-pointer text-gray-800 underline"
-          onClick={() => navigate('register')}
+          onClick={() => navigate('login')}
         >
-          Sign up
+          Sign in
         </button>
       </p>
     </AuthContainer>
