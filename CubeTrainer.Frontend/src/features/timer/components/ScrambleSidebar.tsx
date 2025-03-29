47af63a560c140ce0590b2a0ac5c80e5b75779fe
Cubball <138378823+Cubball@users.noreply.github.com>
2025-03-29 12:10:12 +0200

    refactor: extract ScrambleSidebar component

---
+++ CubeTrainer.Frontend/src/features/timer/components/ScrambleSidebar.tsx
@@ -0,0 +1,52 @@
+import ScrambleView from './ScrambleView'
+
+export interface ScrambleSidebarProps {
+  scramble: string
+  hint?: string
+  hintVisible: boolean
+  setHintVisible: (visible: boolean) => void
+  onRegenerateClick: () => void
+}
+
+const ScrambleSidebar = ({
+  scramble,
+  hint,
+  hintVisible,
+  setHintVisible,
+  onRegenerateClick,
+}: ScrambleSidebarProps) => {
+  return (
+    <div className="flex h-full w-full flex-col items-center justify-center gap-4">
+      <p className="w-full px-4 text-center text-xl font-bold">
+        Scramble:
+        <br />
+        {scramble}
+      </p>
+      <ScrambleView scramble={scramble} />
+      <button
+        className="w-1/2 cursor-pointer rounded-sm bg-gray-800 px-4 py-2 text-white"
+        onClick={onRegenerateClick}
+      >
+        Regenerate
+      </button>
+      <button
+        className="w-1/2 cursor-pointer rounded-sm bg-gray-800 px-4 py-2 text-white"
+        onClick={() => setHintVisible(!hintVisible)}
+      >
+        {hintVisible ? 'Hide hint' : 'Show hint'}
+      </button>
+      <p
+        className={
+          'w-full px-4 text-center text-xl font-bold' +
+          (hintVisible ? '' : ' opacity-0')
+        }
+      >
+        Hint:
+        <br />
+        {hint ?? "You haven't seleceted an algorithm for this case!"}
+      </p>
+    </div>
+  )
+}
+
+export default ScrambleSidebar
--- CubeTrainer.Frontend/src/features/timer/pages/Trainer.tsx
+++ CubeTrainer.Frontend/src/features/timer/pages/Trainer.tsx
@@ -1,10 +1,10 @@
 import { useQuery } from '@tanstack/react-query'
 import { useAxiosWithAuth } from '../../../lib/axios'
-import ScrambleView from '../components/ScrambleView'
 import { useState } from 'react'
 import Stopwatch from '../components/Stopwatch'
 import Error from '../../../components/Error'
 import Loader from '../../../components/Loader'
+import ScrambleSidebar from '../components/ScrambleSidebar'
 
 const RANDOM_SCRAMBLE_QUERY_KEY = 'random-scramble'
 
@@ -45,36 +45,14 @@ const Trainer = () => {
   const scramble = data?.data.scramble.moves ?? ''
   return (
     <div className="flex flex-1 flex-col lg:flex-row">
-      <div className="flex h-full flex-col items-center justify-center gap-4 lg:basis-1/3">
-        <p className="w-full px-4 text-center text-xl font-bold">
-          Scramble:
-          <br />
-          {scramble}
-        </p>
-        <ScrambleView scramble={scramble} />
-        <button
-          className="w-1/2 cursor-pointer rounded-sm bg-gray-800 px-4 py-2 text-white"
-          onClick={() => refetch()}
-        >
-          Regenerate
-        </button>
-        <button
-          className="w-1/2 cursor-pointer rounded-sm bg-gray-800 px-4 py-2 text-white"
-          onClick={() => setHintVisible((visible) => !visible)}
-        >
-          {hintVisible ? 'Hide hint' : 'Show hint'}
-        </button>
-        <p
-          className={
-            'w-full px-4 text-center text-xl font-bold' +
-            (hintVisible ? '' : ' opacity-0')
-          }
-        >
-          Hint:
-          <br />
-          {data?.data.scramble.case.selectedAlgorithm?.moves ??
-            "You haven't seleceted an algorithm for this case!"}
-        </p>
+      <div className="lg:w-1/3">
+        <ScrambleSidebar
+          hintVisible={hintVisible}
+          setHintVisible={setHintVisible}
+          scramble={scramble}
+          hint={data?.data.scramble.case.selectedAlgorithm?.moves}
+          onRegenerateClick={refetch}
+        />
       </div>
       <div className="flex flex-1 items-center justify-center rounded-lg border-2 border-gray-800 p-4 text-5xl font-bold">
         <Stopwatch onStop={() => setHintVisible(false)} />
