f94e19e571357c8a33d16ba0346c0b0f165a3f21
Cubball <138378823+Cubball@users.noreply.github.com>
2026-05-17 19:53:32 +0300

    fix: tweak initial cost config values

--- CubeTrainer.Cube/Analyzer/CostConfig.cs
+++ CubeTrainer.Cube/Analyzer/CostConfig.cs
@@ -32,15 +32,15 @@ internal static class CostConfig
 
     public static double RingPullCost { get; } = 2.0;
 
-    public static double RingPushCost { get; } = 2.0;
+    public static double RingPushCost { get; } = 2.5;
 
     public static double DoubleRingPullCost { get; } = 2.5;
 
-    public static double MiddlePullCost { get; } = 2.0;
+    public static double MiddlePullCost { get; } = 2.5;
 
-    public static double MiddlePushCost { get; } = 2.5;
+    public static double MiddlePushCost { get; } = 3.0;
 
-    public static double DoubleMiddlePullCost { get; } = 4.0;
+    public static double DoubleMiddlePullCost { get; } = 5.0;
 
     public static double SliceMoveMultiplier { get; } = 1.5;
 }
