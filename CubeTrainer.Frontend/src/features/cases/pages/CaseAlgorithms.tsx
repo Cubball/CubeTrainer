63a7145ca2c858879fa4b1e6ec1a7d7480efd9b7
Cubball <138378823+Cubball@users.noreply.github.com>
2026-05-29 03:13:42 +0300

    feat: Create new algorithm button on case algs list

--- CubeTrainer.Frontend/src/features/cases/pages/CaseAlgorithms.tsx
+++ CubeTrainer.Frontend/src/features/cases/pages/CaseAlgorithms.tsx
@@ -148,21 +148,19 @@ const CaseAlgorithms = () => {
             />
           </div>
         </div>
+        <Link
+          to={`/cases/${caseData?.data.case.id}/algorithms/new`}
+          className="w-fit cursor-pointer rounded-sm bg-gray-800 px-4 py-2 text-center text-white"
+        >
+          Create a New Algorithm
+        </Link>
         <div className="max-w-7xl min-w-md md:w-3/4">
           {algorithms.length === 0 && (
-            <div className="flex w-full flex-col items-center gap-4">
-              <h2 className="text-center text-xl text-gray-500 italic">
-                There are no algorithms for this case
-              </h2>
-              <Link
-                to={`/cases/${caseData?.data.case.id}/algorithms/new`}
-                className="w-fit cursor-pointer rounded-sm bg-gray-800 px-4 py-2 text-center text-white"
-              >
-                Create a New Algorithm
-              </Link>
-            </div>
+            <h2 className="text-center text-xl text-gray-500 italic">
+              There are no algorithms for this case
+            </h2>
           )}
-          {algorithms.length > 0 && (
+           {algorithms.length > 0 && (
             <table className="w-full border-2 border-gray-800">
               <thead>
                 <tr className="bg-gray-800 text-white *:p-2">
